1883408964bfc70151dd5ff3f3abd9445cb43d4e
fez <eugenefezeu@gmail.com>
2019-05-19 12:09:15 +0100

    fichier oublier

---
+++ src/relations/relations.cpp
@@ -0,0 +1,73 @@
+#include "relations.h"
+
+Client::Client  gestionClient;
+Article::Article  gestionArticle;
+Livraison::Livraison gestionLivraison;
+Commande::Commande gestionCommande;
+
+/*
+    @name: client_article
+    @brief: cette fonction donne la liste de client d'un article;
+    @params: idArticles id de l'article dont on veut les clients 
+    @return: retourne une liste de cleint s'il y a des clients ou une liste vide
+*/
+
+Liste<Client::Base> client_article(unsigned long idArticle){
+    Liste<Client::Base> clients;
+    Liste<Commande::Base> cmds = gestionCommande.commandes_pour(idArticle);
+    if(!cmds.est_vide()){
+        Cellule<Commande::Base> * i = cmds.recup_tete();
+        while ( i!=cmds.recup_sentinelle())
+        {
+            clients.ajouter_trie(gestionClient.chercher_client(i->get().ref()));
+        }
+        
+    }
+    return clients;
+};
+
+/*
+    @name: article_client
+    @brief: cette fonction donne la liste des article achete par un client 
+    @params: idClient id du client dont on veut les articles 
+    @return: retourne une liste d'article s'il y a des articles ou une liste vide
+*/
+Liste<Article::Base> article_client(unsigned long idClient){
+    Liste<Article::Base> article;
+    Liste<Commande::Base>  cmds = gestionCommande.commandes_de(idClient);
+    if(!cmds.est_vide()){
+        Cellule<Commande::Base> * i = cmds.recup_tete();
+        while ( i!=cmds.recup_sentinelle())
+        {
+            article.ajouter_trie(gestionArticle.chercher(i->get().ref()));
+        }
+    }
+    return article;
+}
+
+/*
+
+*/
+int commander_article(const char * nomClient ,const char * libelle, unsigned long quantite){
+    Cellule<Client::Base> * pc = gestionClient.chercher_client(nomClient);
+    if(pc == NULL){
+        return CLIENT_PAS_TROUVER;
+    }
+    Cellule<Article::Base> * pa = gestionArticle.chercher(libelle);
+    if(pa == NULL){
+        return ARTICLE_PAS_TROUVER;
+    }
+    if(pa->get().tester_quantite(quantite) < 0){
+        return PAS_SUFFISANT;
+    }
+    pa->get().ajouter_quantite(-quantite);
+    int code = gestionCommande.creer(pc->get().get_numero(),pa->get().get_reference(),quantite);
+    if(code == ERROR_CODE){
+        return ERREUR_SYSTEME;
+    }
+    code = gestionLivraison.creer(code,pc->get().get_numero(),quantite);
+    if(code == ERROR_CODE){
+        return ERREUR_SYSTEME;
+    }
+    return PAS_DERREUR;
+};
---
+++ src/relations/relations.h
@@ -0,0 +1,15 @@
+#ifndef RELATIONS
+#define RELATIONS
+#include "../Client/Clients.h"
+#include "../Article/Articles.h"
+#include "../Article/Articles.h"
+#include "../Commande/Commandes.h"
+#include "../Livraison/Livraisons.h"
+
+Liste<Client::Base> client_article(typeId);
+Liste<Article::Base> article_client(typeId);
+int commander_article(const char *,const char *, unsigned long);
+int commander_article(typeId,typeId, unsigned long);
+int commander_article(const char *,typeId, unsigned long); 
+int commander_article(typeId,const char *, unsigned long);
+#endif
